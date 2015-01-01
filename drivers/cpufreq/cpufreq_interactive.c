2c31f1ad888a4dde933ea269a4b8c1e02c28e4a1
franciscofranco <franciscofranco.1990@gmail.com>
2015-01-01 15:15:43 +0000

    cpufreq: interactive: don't scale up if load is <= 5%, it's not worth it
    Change-Id: Iba180efa1c9c4b2751de2d09a7a4d3fe920977af
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -118,6 +118,8 @@ static bool io_is_busy;
  */
 static unsigned long max_freq_hysteresis = 0;
 
+#define DOWN_LOW_LOAD_THRESHOLD 5
+
 /* Round to starting jiffy of next evaluation window */
 static u64 round_to_nw_start(u64 jif)
 {
@@ -375,6 +377,8 @@ static void cpufreq_interactive_timer(unsigned long data)
 			if (new_freq < hispeed_freq)
 				new_freq = hispeed_freq;
 		}
+	} else if (cpu_load <= DOWN_LOW_LOAD_THRESHOLD) {
+		new_freq = pcpu->policy->cpuinfo.min_freq;
 	} else {
 		new_freq = choose_freq(pcpu, loadadjfreq);
 	}
