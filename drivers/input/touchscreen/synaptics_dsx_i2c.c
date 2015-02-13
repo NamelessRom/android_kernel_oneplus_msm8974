955622f4b4ad2851dfdfc12baa570bfc65ee5fd5
Alexander Martinz <eviscerationls@gmail.com>
2015-02-13 14:14:32 +0100

    input: add dialer screen off gesture
      * needs to be handled at userspace by keyhandler
      * keycode is 249
      * enable/disable via /proc/touchpanel/dialer_enable
    
    Change-Id: I2644386f736196ea8520b1c41521d86f45ff60bb
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -1388,6 +1388,33 @@ static int synaptics_rmi4_proc_flashlight_write(struct file *filp, const char __
 	return len;
 }
 
+static int synaptics_rmi4_proc_dialer_read(char *page, char **start, off_t off,
+		int count, int *eof, void *data)
+{
+	return sprintf(page, "%d\n", atomic_read(&syna_rmi4_data->dialer_enable));
+}
+
+static int synaptics_rmi4_proc_dialer_write(struct file *filp, const char __user *buff,
+		unsigned long len, void *data)
+{
+	int enable;
+	char buf[2];
+
+	if (len > 2)
+		return 0;
+
+	if (copy_from_user(buf, buff, len)) {
+		print_ts(TS_DEBUG, KERN_ERR "Read proc input error.\n");
+		return -EFAULT;
+	}
+
+	enable = (buf[0] == '0') ? 0 : 1;
+
+	atomic_set(&syna_rmi4_data->dialer_enable, enable);
+
+	return len;
+}
+
 //smartcover proc read function
 static int synaptics_rmi4_proc_smartcover_read(char *page, char **start, off_t off,
 		int count, int *eof, void *data) {
@@ -1658,6 +1685,13 @@ static int synaptics_rmi4_init_touchpanel_proc(void)
 	if (proc_entry) {
 		proc_entry->write_proc = synaptics_rmi4_proc_flashlight_write;
 		proc_entry->read_proc = synaptics_rmi4_proc_flashlight_read;
+	}
+
+	// wake to dialer
+	proc_entry = create_proc_entry("dialer_enable", 0664, procdir);
+	if (proc_entry) {
+		proc_entry->write_proc = synaptics_rmi4_proc_dialer_write;
+		proc_entry->read_proc = synaptics_rmi4_proc_dialer_read;
 	}
 
 	//for pdoze enable/disable interface
@@ -2402,6 +2436,8 @@ static unsigned char synaptics_rmi4_update_gesture2(unsigned char *gesture,
 			switch (gesture[2]) {
 				case 0x01:  //UP
 					gesturemode = DownVee;
+					if (atomic_read(&syna_rmi4_data->dialer_enable))
+						keyvalue = KEY_GESTURE_V_UP;
 					break;
 				case 0x02:  //DOWN
 					gesturemode = UpVee;
@@ -3812,6 +3848,7 @@ static void synaptics_rmi4_set_params(struct synaptics_rmi4_data *rmi4_data)
 	set_bit(KEY_GESTURE_CIRCLE, rmi4_data->input_dev->keybit);
 	set_bit(KEY_GESTURE_SWIPE_DOWN, rmi4_data->input_dev->keybit);
 	set_bit(KEY_GESTURE_V, rmi4_data->input_dev->keybit);
+	set_bit(KEY_GESTURE_V_UP, rmi4_data->input_dev->keybit);
 	set_bit(KEY_GESTURE_LTR, rmi4_data->input_dev->keybit);
 	set_bit(KEY_GESTURE_GTR, rmi4_data->input_dev->keybit);
 	synaptics_ts_init_virtual_key(rmi4_data);
@@ -3899,6 +3936,7 @@ static int synaptics_rmi4_set_input_dev(struct synaptics_rmi4_data *rmi4_data)
 	atomic_set(&rmi4_data->camera_enable, 0);
 	atomic_set(&rmi4_data->music_enable, 0);
 	atomic_set(&rmi4_data->flashlight_enable, 0);
+	atomic_set(&rmi4_data->dialer_enable, 0);
 
 	rmi4_data->glove_enable = 0;
 	rmi4_data->pdoze_enable = 0;
@@ -4679,6 +4717,7 @@ static int synaptics_rmi4_suspend(struct device *dev)
 			atomic_read(&rmi4_data->double_tap_enable) ||
 			atomic_read(&rmi4_data->camera_enable) ||
 			atomic_read(&rmi4_data->music_enable) ||
+			atomic_read(&rmi4_data->dialer_enable) ||
 			atomic_read(&rmi4_data->flashlight_enable) ? 1 : 0);
 
 	if (atomic_read(&rmi4_data->syna_use_gesture) || rmi4_data->pdoze_enable) {
@@ -4742,6 +4781,7 @@ static int synaptics_rmi4_resume(struct device *dev)
 			atomic_read(&rmi4_data->double_tap_enable) ||
 			atomic_read(&rmi4_data->camera_enable) ||
 			atomic_read(&rmi4_data->music_enable) ||
+			atomic_read(&rmi4_data->dialer_enable) ||
 			atomic_read(&rmi4_data->flashlight_enable) ? 1 : 0);
 		rmi4_data->pwrrunning = false;
 		return 0;
