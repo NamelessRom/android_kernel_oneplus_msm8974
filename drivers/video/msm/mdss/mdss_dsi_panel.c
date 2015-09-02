f1c26cf3c9d327923f1b91c4e45adec49a98bad2
morckx <morckx@gmail.com>
2015-09-02 09:11:07 +0200

    video: mdss: mimic OmniROM's panel-reset on the find7s
    Omni's procedure seems to be the most robust one.
    
    Change-Id: Id986ec2496896e011d3900591d8b41d90b922c52
    Signed-off-by: morckx <morckx@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -278,24 +278,35 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 				gpio_set_value((ctrl_pdata->rst_gpio),
 					pdata->panel_info.rst_seq[i]);
 				if (pdata->panel_info.rst_seq[++i])
-					usleep(pinfo->rst_seq[i] * 1000);
+					mdelay(pinfo->rst_seq[i]);
 			}
 
 			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
 				gpio_set_value((ctrl_pdata->disp_en_gpio), 1);
 #ifdef CONFIG_MACH_OPPO
-			usleep(2 * 1000);
-			if (gpio_is_valid(ctrl_pdata->lcd_5v_en_gpio))
-				gpio_direction_output(ctrl_pdata->lcd_5v_en_gpio, 1);
+			gpio_direction_output((ctrl_pdata->disp_en_gpio),1);
+			mdelay(2);
 #endif
 		}
 
+#ifdef CONFIG_MACH_OPPO
+		gpio_direction_output((ctrl_pdata->lcd_5v_en_gpio),1);
+		mdelay(10);
+		gpio_direction_output((ctrl_pdata->lcd_5v_en_gpio),0);
+		mdelay(2);
+		gpio_direction_output((ctrl_pdata->disp_en_gpio),0);
+		mdelay(10);
+		gpio_direction_output((ctrl_pdata->disp_en_gpio),1);
+		mdelay(2);
+		gpio_direction_output((ctrl_pdata->lcd_5v_en_gpio),1);
+#else
 		if (gpio_is_valid(ctrl_pdata->mode_gpio)) {
 			if (pinfo->mode_gpio_state == MODE_GPIO_HIGH)
 				gpio_set_value((ctrl_pdata->mode_gpio), 1);
 			else if (pinfo->mode_gpio_state == MODE_GPIO_LOW)
 				gpio_set_value((ctrl_pdata->mode_gpio), 0);
 		}
+#endif
 		if (ctrl_pdata->ctrl_state & CTRL_STATE_PANEL_INIT) {
 			pr_debug("%s: Panel Not properly turned OFF\n",
 						__func__);
@@ -305,22 +316,26 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	} else {
 #ifdef CONFIG_MACH_OPPO
 		gpio_set_value((ctrl_pdata->rst_gpio), 1);
-		usleep(110 * 1000);
+		mdelay(5);
 #endif
 		gpio_set_value((ctrl_pdata->rst_gpio), 0);
-		gpio_free(ctrl_pdata->rst_gpio);
 #ifdef CONFIG_MACH_OPPO
-		usleep(10 * 1000);
-		if (gpio_is_valid(ctrl_pdata->lcd_5v_en_gpio))
-			gpio_direction_output(ctrl_pdata->lcd_5v_en_gpio, 0);
-		usleep(2 * 1000);
+		mdelay(10);
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
+		mdelay(10);
+		gpio_direction_output((ctrl_pdata->lcd_5v_en_gpio), 0);
+		mdelay(2);
 #endif
+		gpio_free(ctrl_pdata->rst_gpio);
 		if (gpio_is_valid(ctrl_pdata->disp_en_gpio)) {
 			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
+#ifdef CONFIG_MACH_OPPO
+			gpio_direction_output((ctrl_pdata->disp_en_gpio),0);
+#endif
 			gpio_free(ctrl_pdata->disp_en_gpio);
 		}
 #ifdef CONFIG_MACH_OPPO
-		usleep(10 * 1000);
+		mdelay(10);
 #endif
 		if (gpio_is_valid(ctrl_pdata->mode_gpio))
 			gpio_free(ctrl_pdata->mode_gpio);
