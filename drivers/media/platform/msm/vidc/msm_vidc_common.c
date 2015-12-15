e8f744698d9179503f18c51b85321b38103adbc0
myfluxi <linflux@arcor.de>
2015-12-15 23:52:13 +0100

    Revert "msm: vidc: fix deadlock in video driver"
    This reverts commit 052b0f7103056c2d0269ef4a9f07f0c01677d7eb.
    
    Change-Id: Id20385fdc9952e235463f9336960f2d5ec83f538

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -977,6 +977,7 @@ void msm_comm_session_clean(struct msm_vidc_inst *inst)
 	}
 
 	hdev = inst->core->device;
+	mutex_lock(&inst->lock);
 	if (hdev && inst->session) {
 		dprintk(VIDC_DBG, "cleaning up instance: 0x%p\n", inst);
 		rc = call_hfi_op(hdev, session_clean,
@@ -987,6 +988,7 @@ void msm_comm_session_clean(struct msm_vidc_inst *inst)
 		}
 		inst->session = NULL;
 	}
+	mutex_unlock(&inst->lock);
 }
 
 static void handle_session_close(enum command_response cmd, void *data)
